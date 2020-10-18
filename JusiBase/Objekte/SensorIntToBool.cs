f957a571f81a489e4984529948a1db0cbe3aff9a
Michael Meienberg <public@meienberg.cc>
2020-10-18 22:52:45 +0200

    neuer sensortyp

---
+++ JusiBase/Objekte/SensorIntToBool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JusiBase
+{
+    public class SensorIntToBool : SensorBool
+    {
+        public SensorIntToBool(string objektId) : base(objektId)
+        {
+
+        }
+
+        public override void Update()
+        {
+            IOBrokerJSONGet jsonResult = clusterConn.GetIOBrokerValue(ObjektId);
+            if (jsonResult == null && jsonResult.valInt != null)
+            {
+                Console.WriteLine("keine Daten erhalten");
+                return;
+            }
+            if (jsonResult.valInt.Value == 255)
+            {
+                Status = true;
+            }
+            else
+            {
+                Status = false;
+            }
+        }
+    }
+}
