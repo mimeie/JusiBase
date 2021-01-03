79f9e58a05ab9885796dea8aa50f991392aa9cc9
Michael Meienberg <public@meienberg.cc>
2021-01-03 11:07:06 +0100

    job manager eingefügt, ohne job logik

---
+++ JusiBase/JobManager/JobManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Quartz;
+using Quartz.Impl;
+
+namespace JusiBase
+{
+    public class JobManager : IDisposable
+    {
+
+        private IScheduler _scheduler;
+        private StdSchedulerFactory _factory;
+
+        public JobManager()
+        {
+
+        }
+
+        public async void Initialize()
+        {
+            _factory = new StdSchedulerFactory();
+            _scheduler = await _factory.GetScheduler();
+
+            await _scheduler.Start();
+
+            //InitializeJobs();
+        }
+
+        public void Dispose()
+        {
+            if (_scheduler != null)
+            {
+                _scheduler.Shutdown();
+                _scheduler = null;
+            }
+        }
+
+        //private void InitializeJobs()
+        //{
+        //    try
+        //    {
+
+
+        //        InitializeFeuchtigkeitPollJob();
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Jobs konnten nicht initialisiert werden: {0}", ex);
+        //        throw;
+        //    }
+        //}
+
+
+        //private void InitializeFeuchtigkeitPollJob()
+        //{
+        //    IJobDetail feuchtigkeitPollJob = JobBuilder.Create<FeuchtigkeitPollJob>()
+        //        .WithIdentity("FeuchtigkeitPollJob")
+        //        .Build();
+
+        //    // Trigger the job to run now, and then repeat every x seconds
+        //    ITrigger feuchtigkeitPollTrigger = TriggerBuilder.Create()
+        //        .WithIdentity("FeuchtigkeitPollJobTrigger")
+        //        .StartNow()
+        //        .WithSimpleSchedule(x => x
+        //            .WithIntervalInMinutes(SteuerungLogic.Instance.KellerSensor.CheckIntervallMinutes)
+        //            .RepeatForever())
+        //        .Build();
+
+        //    _scheduler.ScheduleJob(feuchtigkeitPollJob, feuchtigkeitPollTrigger);
+        //    Console.WriteLine("FeuchtigkeitPollJob mit Interval {0}min gestartet", SteuerungLogic.Instance.KellerSensor.CheckIntervallMinutes);
+        //}
+
+
+
+    }
+}
---
+++ JusiBaseTest/JobManagerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JusiBase;
+
+namespace JusiBaseTest
+{
+    [TestClass]
+    public class JobManagerTest
+    {
+        private JobManager _jobManager;
+
+        [TestMethod]
+        public void TestJobManager()
+        {
+            try
+            {
+                _jobManager = new JobManager();
+                _jobManager.Initialize();
+
+                //InitializeFeuchtigkeitPollJob();
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fehler beim Testen des job managers", ex);
+                //throw;
+            }
+
+        }
+
+
+        //private void InitializeFeuchtigkeitPollJob()
+        //{
+        //    IJobDetail feuchtigkeitPollJob = JobBuilder.Create<FeuchtigkeitPollJob>()
+        //        .WithIdentity("FeuchtigkeitPollJob")
+        //        .Build();
+
+        //    // Trigger the job to run now, and then repeat every x seconds
+        //    ITrigger feuchtigkeitPollTrigger = TriggerBuilder.Create()
+        //        .WithIdentity("FeuchtigkeitPollJobTrigger")
+        //        .StartNow()
+        //        .WithSimpleSchedule(x => x
+        //            .WithIntervalInMinutes(10)
+        //            .RepeatForever())
+        //        .Build();
+
+        //    _scheduler.ScheduleJob(feuchtigkeitPollJob, feuchtigkeitPollTrigger);
+        //    Console.WriteLine("FeuchtigkeitPollJob mit Interval {0}min gestartet", 11);
+        //}
+    }
+}
